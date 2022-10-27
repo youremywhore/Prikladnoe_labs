cffc62f92b47f0530a454769da9cccf135ed4296
y <95417496+chief-keef300@users.noreply.github.com>
2022-10-27 12:32:02 +0300

    lab4 is done

---
+++ ShopApi.Repository/CompanyRepository.cs
@@ -0,0 +1,24 @@
+﻿using ShopApi.Contracts;
+using ShopApi.Entities;
+using ShopApi.Entities.Models;
+
+namespace ShopApi.Repository;
+
+public class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
+{
+    public CompanyRepository(RepositoryContext repositoryContext)
+        : base(repositoryContext)
+    {
+    }
+
+    public IEnumerable<Company> GetAllCompanies(bool trackChanges) =>
+        FindAll(trackChanges)
+            .OrderBy(c => c.Name)
+            .ToList();
+    
+    public Company? GetCompany(Guid companyId, bool trackChanges)
+    {
+        return FindByCondition(c
+            => c.Id.Equals(companyId), trackChanges).SingleOrDefault();
+    }
+}
---
+++ ShopApi.Web.Api/Controllers/CompaniesController.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using ShopApi.Contracts;
+using ShopApi.Entities.DataTransferObjects;
+
+namespace ShopApi.Web.Api.Controllers;
+
+[Route("api/companies")]
+[ApiController]
+public class CompaniesController : ControllerBase
+{
+    private readonly IRepositoryManager _repository;
+    private readonly ILoggerManager _logger;
+    private readonly IMapper _mapper;
+
+    public CompaniesController(IRepositoryManager repository, ILoggerManager
+        logger, IMapper mapper)
+    {
+        _repository = repository;
+        _logger = logger;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public IActionResult GetCompanies()
+    {
+        var companies = _repository.Company.GetAllCompanies(trackChanges: false);
+        var companiesDto = _mapper.Map<IEnumerable<CompanyDto>>(companies);
+        return Ok(companiesDto);
+    }
+    
+    [HttpGet("{id:guid}")]
+    public IActionResult GetCompany(Guid id)
+    {
+        var company = _repository.Company.GetCompany(id, trackChanges: false);
+        if (company == null)
+        {
+            _logger.LogInfo($"Company with id: {id} doesn't exist in the database.");
+            return NotFound();
+        }
+        else
+        {
+            var companyDto = _mapper.Map<CompanyDto>(company);
+            return Ok(companyDto);
+        }
+    }
+}
